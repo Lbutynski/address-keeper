8283746bc2b19c99d07b1db5aaad5fc4504022ec
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2024-05-04 18:27:56 +0200

    fix: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@ import Link from "next/link"
 
 export const Header = () => (
   <div className="w-full">
-    <ul className="flex flex-row w-full  m-5 justify-between">
+    <ul className="flex flex-row w-full  m-5 ">
       <Link href="/">
-        <li className="p-5 bg-blue-500 rounded-md text-white">Home</li>
+        <li className="p-5 bg-blue-500 hover:bg-blue-600 rounded-md text-white">
+          Address Keeper
+        </li>
       </Link>
       <Link href="/create">
-        <li className="p-5 bg-blue-500 rounded-md text-white ">Create</li>
+        <li className="p-5 bg-blue-500 hover:bg-blue-600 rounded-md text-white mx-10">
+          Create
+        </li>
       </Link>
     </ul>
   </div>
